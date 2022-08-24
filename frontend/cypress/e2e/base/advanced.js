43d8334b9612ae8fe42fd0237227d1706a43f4ef
toru2220 <toru2220@gmail.com>
2022-08-24 17:46:45 +0900

    fix

---
+++ frontend/cypress/e2e/base/advanced.js
@@ -0,0 +1,8 @@
+describe('test_churadata_hp', function() {
+    it('test_recruitment', function() {
+     cy.viewport(1440, 798);
+     // churadataのHPにアクセス
+     cy.visit('https://www.google.co.jp/');
+
+    })
+})
